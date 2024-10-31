c2441b4f5dd6d11e1ad04e9ad164a64219e5e230
Alejandro de Jesús Escareño Flores <alejandroef04@gmail.com>
2024-10-30 20:41:11 -0600

    🏗️ Reactive Forms
    Writing a basic reactive form and its validation

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+const product = {
+    name: 'Test',
+    price: 100,
+    inStorage: 10
+}
 
 @Component({
-  templateUrl: './basic-page.component.html',
-  styles: ``
+    templateUrl: './basic-page.component.html',
+    styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
+    // importante importar ReactiveFormsModule en el módulo
+    // public myForm: FormGroup = new FormGroup({
+    //     name: new FormControl(''),
+    //     price: new FormControl(0),
+    //     inStorage: new FormControl(0),
+    // });
+
+    public myForm: FormGroup = this.fb.group({
+        name: ['', [ Validators.required, Validators.minLength(3) ]],
+        price: [0, [ Validators.required, Validators.min(0) ]],
+        inStorage: [0, [ Validators.required, Validators.min(0) ]]
+    });
+
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.myForm.reset(product);
+    }
+
+    onSave(): void {
+
+        if(this.myForm.invalid) return;
+
+        this.myForm.reset({ price: 0, inStorage: 0 });
+    }
 }
