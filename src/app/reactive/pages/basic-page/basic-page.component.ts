4209bf9ef9b9dadbdfaa0ac413ec2e418f53ac4f
Alejandro de Jesús Escareño Flores <alejandroef04@gmail.com>
2024-10-31 21:13:51 -0600

    🔖 Finishing reactive dynamic Forms
    Finishing reactive dynamic forms in angular with Validators

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -5,7 +5,7 @@ const product = {
     name: 'Test',
     price: 100,
     inStorage: 10
-}
+};
 
 @Component({
     templateUrl: './basic-page.component.html',
@@ -21,21 +21,48 @@ export class BasicPageComponent implements OnInit {
     // });
 
     public myForm: FormGroup = this.fb.group({
-        name: ['', [ Validators.required, Validators.minLength(3) ]],
-        price: [0, [ Validators.required, Validators.min(0) ]],
-        inStorage: [0, [ Validators.required, Validators.min(0) ]]
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        price: [0, [Validators.required, Validators.min(0)]],
+        inStorage: [0, [Validators.required, Validators.min(0)]]
     });
 
-    constructor(private fb: FormBuilder) {}
+    constructor(private fb: FormBuilder) { }
 
     ngOnInit(): void {
-        this.myForm.reset(product);
+        // this.myForm.reset(product);
+    }
+
+    isValidField(field: string): boolean | null {
+        return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+    }
+
+    getFieldError(field: string): string | null {
+
+        if (!this.myForm.controls[field]) return null;
+
+        const errors = this.myForm.controls[field].errors || {};
+
+        for (const key of Object.keys(errors)) {
+
+            switch (key) {
+                case 'required':
+                    return 'El campo es requerido';
+                case 'minlength':
+                    return `El campo debe tener al menos ${errors['minlength'].requiredLength} caracteres`;
+            }
+
+        }
+
+        return null;
     }
 
     onSave(): void {
 
-        if(this.myForm.invalid) return;
+        if (this.myForm.invalid) {
+            this.myForm.markAllAsTouched(); // Marca todos los campos como tocados para mostrar los errores al enviar
+            return;
+        };
 
         this.myForm.reset({ price: 0, inStorage: 0 });
-    }
+    };
 }
--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,80 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './dynamic-page.component.html',
     styles: ``
 })
 export class DynamicPageComponent {
+
+    public myForm: FormGroup = this.fb.group({
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        favoriteGames: this.fb.array([
+            ['Metal Gear', Validators.required],
+            ['Zelda', Validators.required]
+        ]),
+    });
+
+    public newFavoriteGame: FormControl = this.fb.control('', [Validators.required, Validators.minLength(3)]);
+
+    constructor(private fb: FormBuilder) { }
+
+    get favoriteGames() {
+        return this.myForm.get('favoriteGames') as FormArray;
+    }
+
+    isValidFieldInArray(formArray: FormArray, index: number) {
+        return formArray.controls[index].errors && formArray.controls[index].touched;
+    }
+
+
+    isValidField(field: string): boolean | null {
+        return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+    }
+
+    getFieldError(field: string): string | null {
+
+        if (!this.myForm.controls[field]) return null;
+
+        const errors = this.myForm.controls[field].errors || {};
+
+        for (const key of Object.keys(errors)) {
+
+            switch (key) {
+                case 'required':
+                    return 'El campo es requerido';
+                case 'minlength':
+                    return `El campo debe tener al menos ${errors['minlength'].requiredLength} caracteres`;
+            }
+
+        }
+
+        return null;
+    }
+
+    addToFavoriteGames(): void {
+        if (this.newFavoriteGame.invalid) return;
+        const newGame = this.newFavoriteGame.value;
+
+        this.favoriteGames.push(this.fb.control(newGame, Validators.required));
+
+        this.newFavoriteGame.reset();
+    }
+
+    onDeleteFav(index: number): void {
+        this.favoriteGames.removeAt(index);
+    }
+
+    onSubmit(): void {
+
+        if (this.myForm.invalid) {
+            this.myForm.markAllAsTouched();
+            return;
+        }
+
+        console.log(this.myForm.value);
+        this.myForm.reset();
+        (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([]);
+    }
 
 }
